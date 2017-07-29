ffc59508b36be1dc36539dab82c724187c8840cc
Joel Sullivan <joel-s@users.noreply.github.com>
2017-07-28 20:55:54 -0400

    Fix recursion.py (it works) and make it more elegant

--- Python/Exercises/Recursion/recursion.py
+++ Python/Exercises/Recursion/recursion.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
 """
-for each item:
-    add to next "empty branch" in tree
+Solves the problem described in the README, recursively.
 
-def show_tree(tree):
-    if tree.root:
-        print(tree.root)
-    if left:
-        show_tree(left)
-    if right:
-        show_tree(right)
+Usage:
+    python3 recursion.py <input.txt >actual.txt
+    diff -c output.txt actual.txt    # compare expected output to actual 
+
 """
 
 import sys
@@ -18,84 +14,80 @@ import os
 
 def main():
     values = sys.stdin.readline().split(",")
+    values = map(int, values)
     create_and_show_tree(values)
 
-def depth_test():
-    assert(create_and_show_tree([1]) == 1)
-    assert(create_and_show_tree([1, 2]) == 2)
 
 def create_and_show_tree(values):
     t = Tree()
-    for item in values:
-        val = int(item)
+    for val in values:
         t.add_breadth_first(val)
-
     t.show_depth_first()
 
 
 class Tree:
-    def __init__(self, val=None):
-        self.root = val
-        self.left = None
-        self.right = None
+    """
+    A binary tree containing 0 or more nodes
+    """
+
+    def __init__(self):
+        self.root = None
 
     def add_breadth_first(self, val):
         if self.root is None:
-            self.root = val
-            return
-        elif self.left is None:
-            self.left = Tree(val)
-            return
-        elif self.right is None:
-            self.right = Tree(val)
-            return
-
-        if self.left.size() // 2 <= self.right.size() // 2:
-            self.left.add_breadth_first(val)
+            self.root = Node(val)
         else:
-            self.right.add_breadth_first(val)
-
-    def depth(self):
-        if self.root is None:
-            return 0
-
-        if self.left is None:
-            left_depth = 0
-        else:
-            left_depth = self.left.depth()
-
-        if self.right is None:
-            right_depth = 0
-        else:
-            right_depth = self.right.depth()
-
-        return 1 + max(left_depth, right_depth)
-
-    def size(self):
-        if self.root is None:
-            return 0
-
-        if self.left is None:
-            left_depth = 0
-        else:
-            left_depth = self.left.depth()
-
-        if self.right is None:
-            right_depth = 0
-        else:
-            right_depth = self.right.depth()
-
-        return 1 + left_depth + right_depth
+            self.root.add_breadth_first(val)
 
     def show_depth_first(self):
         if self.root is None:
             return
-        print(self.root)
+        self.root.show_depth_first()
+
+
+class Node:
+    """
+    A node in a binary tree with a value and possibly left and right nodes.
+    """
+
+    def __init__(self, val):
+        self.value = val
+        self.left = None
+        self.right = None
+
+    def add_breadth_first(self, val):
+        if self.left is None:
+            self.left = Node(val)
+            return
+        elif self.right is None:
+            self.right = Node(val)
+            return
+
+        if self.left.min_depth() <= self.right.min_depth():
+            self.left.add_breadth_first(val)
+        else:
+            self.right.add_breadth_first(val)
+
+    def min_depth(self):
+        if self.left is None:
+            return 0
+        else:
+            left_depth = self.left.min_depth()
+
+        if self.right is None:
+            return 0
+        else:
+            right_depth = self.right.min_depth()
+
+        return 1 + min(left_depth, right_depth)
+
+    def show_depth_first(self):
+        print(self.value)
         if self.left is not None:
             self.left.show_depth_first()
         if self.right is not None:
             self.right.show_depth_first()
 
+
 if __name__ == "__main__":
-    #depth_test()
     main()
